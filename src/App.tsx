092447f6590f892000c01bf45748730ec26141c5
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2025-09-07 23:08:39 -0400

    installs daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <main className="container">
-      <div className="text-3xl font-bold underline">Stay Calculator</div> 
+      <div className="flex items-center">
+        <button className="btn mx-auto">Stay Calculator</button>
+      </div>
     </main>
   );
 }
