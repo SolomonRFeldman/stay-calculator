2959dc9870e67e240f48826f3ffa296d23db49c6
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2025-09-11 21:15:20 -0400

    adds basic stays creator

---
+++ src/components/Stay.tsx
@@ -0,0 +1,30 @@
+import { DateRange } from "react-day-picker"
+
+export type Stay = {
+  id: string
+  range?: DateRange
+}
+
+type StayProps = {
+  stay: Stay
+  editStay: () => void
+  deleteStay: () => void
+}
+
+export default function Stay({ stay: { range }, editStay, deleteStay }: StayProps) {
+  return (
+    <tr>
+      <td>
+        <button className="input input-border" onClick={editStay}>
+          {range?.from && range?.to ? `${range.from.toLocaleDateString()} - ${range.to.toLocaleDateString()}` : "Pick a date"}
+        </button>
+      </td>
+      <td>
+        {range?.from && range?.to ? Math.ceil((range.to.getTime() - range.from.getTime()) / (1000 * 3600 * 24) + 1) : 0} days
+      </td>
+      <td>
+        <button className="btn btn-error btn-sm" onClick={deleteStay}>X</button>
+      </td>
+    </tr>
+  )
+}
--- src/components/StayCalculator.tsx
+++ src/components/StayCalculator.tsx
@@ -1,9 +1,12 @@
+import Stays from "./Stays"
+
 export default function StayCalculator() {
   return (
-    <div className="h-screen pt-4">
+    <div className="h-screen pt-4 flex flex-col gap-4 items-center">
       <span className="prose text-center">
         <h1>Stay Calculator</h1>
       </span>
+      <Stays />
    </div>
   )
 }
