983cada194952daab7b3cc6c999db3aecb307ade
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2025-09-13 13:24:52 -0400

    installs prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,13 @@
 import eslint from "@eslint/js"
 import { defineConfig } from "eslint/config"
 import tseslint from "typescript-eslint"
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended"
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  eslintPluginPrettier,
   {
     languageOptions: {
       parserOptions: {
@@ -20,7 +22,6 @@ export default defineConfig(
     files: ["eslint.config.mjs", "vite.config.ts", "src-tauri/**/*.js"],
     extends: [tseslint.configs.disableTypeChecked],
   },
-  {
-    ignores: ["**/src-tauri/**", "src/main.tsx"],
-  }
+  { ignores: ["**/src-tauri/**", "src/main.tsx"] },
+  { rules: { "prettier/prettier": ["error", { semi: false, printWidth: 120, trailingComma: "none" }] } },
 )
