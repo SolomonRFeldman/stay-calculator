bd32a3ec199cbacd98e6f1201dfc2b01483dc62a
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2025-09-15 12:05:04 -0400

    adds bigger padding to top to avoid status bars and front cameras

--- src/components/StayCalculator.tsx
+++ src/components/StayCalculator.tsx
@@ -2,7 +2,7 @@ import Stays from "./Stays"
 
 export default function StayCalculator() {
   return (
-    <div className="h-screen pt-4 flex flex-col gap-4 items-center">
+    <div className="h-screen pt-10 flex flex-col gap-4 items-center">
       <span className="prose text-center">
         <h1>Stay Calculator</h1>
       </span>
